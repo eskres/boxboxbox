247129e75fd201b603e615bcc1871d44f2781221
Einar <einar.skreslett@gmail.com>
2026-05-19 22:28:24 +0100

    Add partial SQLAlchemy models for driver, race, and pit_stop view.

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, Date
+from app.database import Base
+
+class Driver(Base):
+    __tablename__ = "driver"
+
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    abbreviation = Column(String)
+    nationality_country_id = Column(String)
+    date_of_birth = Column(Date)
+
+class Race(Base):
+    __tablename__ = "race"
+
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer)
+    round = Column(Integer)
+    name = Column(String)
+    date = Column(Date)
+
+class PitStop(Base):
+    __tablename__ = "pit_stop"
+
+    race_id = Column(Integer, primary_key=True)
+    driver_id = Column(String, primary_key=True)
+    stop = Column(Integer, primary_key=True)
+    lap = Column(Integer)
+    time = Column(String)
+    time_millis = Column(Integer)
+    constructor_id = Column(String)
+
+    __table_args__ = {'info': {'is_view': True}}
