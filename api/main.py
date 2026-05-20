edc85c7f5c6870bf421f7994f87b549a31701b95
Einar <einar.skreslett@gmail.com>
2026-05-20 21:23:29 +0100

    Add CORS middleware

--- api/main.py
+++ api/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.resolvers import graphql_app
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(graphql_app, prefix="/graphql")
