a60f35a20cbb4a753d208040f38541bd697dd18d
Einar <einar.skreslett@gmail.com>
2026-05-24 09:03:09 +0100

    Add Redis and define TTL

---
+++ api/app/cache.py
@@ -0,0 +1,15 @@
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+import os
+
+WEEK_TTL = 60 * 60 * 24 * 7
+MONTH_TTL = 60 * 60 * 24 * 30
+
+async def setup_cache():
+    redis = aioredis.from_url(
+        os.getenv("REDIS_URL", "redis://localhost:6379"),
+        encoding="utf8",
+        decode_responses=True
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="f1-cache")
