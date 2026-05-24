f3c97f93a92339c17e67396483578d40ef745be2
Einar <einar.skreslett@gmail.com>
2026-05-24 09:43:41 +0100

    Implemented Redis. Added ALLOWED_ORIGINS to CORS middleware

--- api/main.py
+++ api/main.py
@@ -1,12 +1,20 @@
+import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.resolvers import graphql_app
+from app.cache import setup_cache
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await setup_cache()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(","),
     allow_methods=["*"],
     allow_headers=["*"],
 )
