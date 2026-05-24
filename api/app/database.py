03e71cacda7b6c7d4b777b1b5933a668b9afbd3c
Einar <einar.skreslett@gmail.com>
2026-05-24 09:19:51 +0100

    Added get_context() to bundle session and dataloaders for resolvers

--- api/app/database.py
+++ api/app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from app.dataloader import create_dataloaders
 from dotenv import load_dotenv
+from fastapi import Depends
 import os
 
 load_dotenv()
@@ -22,3 +24,9 @@ class Base(DeclarativeBase):
 async def get_session():
     async with AsyncSessionLocal() as session:
         yield session
+
+async def get_context(session: AsyncSession = Depends(get_session)):
+    return {
+        "session": session,
+        "dataloaders": create_dataloaders(session),
+    }
